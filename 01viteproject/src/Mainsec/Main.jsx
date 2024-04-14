86ab6f3d96937a031af4a1bb941ff3110c808274
Codeitout8103468296 <siddhi.khare.cer22@itbhu.ac.in>
2024-04-14 23:42:57 +0530

    Improved UI and mapping

--- 01viteproject/src/Mainsec/Main.jsx
+++ 01viteproject/src/Mainsec/Main.jsx
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.css';
-
-
-
+import data from "../data.json";
+import Box from './Box';
 
 function Main() {
- 
-   
-    return (
-      <div className='main' id='home' >
-          <div className='home'>
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [datarec, setDatarec] = useState([]);
+
+  useEffect(() => {
+    console.log(data);
+    // setDatarec(data)
+  }, []);
+
+  const submithandler = (event) => {
+    event.preventDefault(); 
+    
+    const filteredData = data.filter(obj => {
+      return (state === "" || obj.state.toLowerCase() === state.toLowerCase()) && (city === "" || obj.city.toLowerCase() === city.toLowerCase());
+    });
+
+    setDatarec(filteredData);
+    console.log(filteredData);
+    useEffect(()=>{
+
+    },[])
+  };
+  
+  return (
+    <div className='main' id='home'>
+      <div className='home'>
         <h1>sahayak</h1>
         <p>"Those who are happiest are those who do the most for others"</p>
-        <form>
-         <label ></label>
-           Name: <input name="myInput"  placeholder='Enter your name' required />
-           <label ></label>
-           State: <input name="myInput"  placeholder='Enter your stae' required />
-           <label ></label>
-           City: <input name="myInput"  placeholder='Enter your city' required />
-      </form>
-      <button  className='btn' type='Submit'>Submit</button>
-        
-        </div>
-        </div>  
-      
-    );
-  }
-  
-  export default Main
+        <form  onSubmit={submithandler}>
+          <div>
+            Name: <input name="myInput" placeholder='Enter your name'  />
+          </div>
+          <div>
+            State: <input onChange={(e) => setState(e.target.value)} value={state} name="myInput" placeholder='Enter your state'  />
+          </div>
+          <div>
+            City: <input onChange={(e) => setCity(e.target.value)} value={city} name="myInput" placeholder='Enter your city'  />
+          </div>
+        </form>
+          <div>
+
+          <button onClick={submithandler} type='submit' className='btn'>Submit</button>
+          </div>
+      </div>
+      <div className='datasec'>
+        {
+          datarec?(datarec.map((obj) => (
+            <Box key={obj.address} {...obj} />
+          ))):(
+            <div>
+              no data
+              </div>
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Main;
--- 01viteproject/src/components/Navbarcom.jsx
+++ 01viteproject/src/components/Navbarcom.jsx
@@ -3,19 +3,38 @@ import './Navbarcom.css';
 import logo from '../assets/image/logo.png'
 import {Link} from 'react-scroll'
 import icon from '../assets/image/icon.png'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navbarcom = () => {
   return (
-     <nav> 
-      <img src={logo} alt='error' className='logo'/>
-      <ul>
-        <li><Link to='home' smooth={true} offset={-260} duration={500}>Home</Link></li>
-        <li><Link to='about' smooth={true} offset={-260} duration={500}>About us</Link></li>
-        <li><Link to='events' smooth={true} offset={-260} duration={500}>Events</Link></li>
-        
-        </ul>
-        <img src={icon} alt="-" className='icon'/>
-     </nav>
+
+
+
+
+<Navbar expand="lg" className='nav' >
+      <Container>
+        <Navbar.Brand href="#home">      <img src={logo} alt='error' className='logo'/>
+</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navmain">
+          <NavDropdown.Item >
+            <Link to='home' smooth={true} offset={-260} duration={500}>Home</Link>
+            </NavDropdown.Item>
+          <NavDropdown.Item >
+        <Link to='about' smooth={true} offset={-260} duration={500}>About us</Link>
+            </NavDropdown.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+     
+
+
+
   )
 }
 
