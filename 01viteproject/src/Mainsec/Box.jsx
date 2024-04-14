b8312727e4dc1b248c56d898709902925ac3e648
Codeitout8103468296 <siddhi.khare.cer22@itbhu.ac.in>
2024-04-14 23:55:52 +0530

    improved UI

--- 01viteproject/src/Mainsec/Box.jsx
+++ 01viteproject/src/Mainsec/Box.jsx
@@ -3,13 +3,22 @@ import Card from 'react-bootstrap/Card';
 
 function Box({organization,address,city,state}) {
   return (
-    <Card style={{ width: '20rem',display:"flex" ,minHeight:"150px"}}>
+    <Card style={{ width: '20rem',display:"flex" ,minHeight:"200px"}}>
       {/* <Card.Img style={{height:'180px'}} variant="top" src={urlToImage} /> */}
-      <Card.Body style={{   display: "flex",flexDirection: "column",
-    justifyContent: "space-between",}}>
-        <div style={{fontSize:"1.2rem",textAlign:"left"}}>Orphanage:<br/>{organization}</div>
+      <Card.Body style={{
+   }}>
+        <div style={{textAlign:"left"}}>
+         <div style={{fontSize:"1.2rem",fontWeight:"600"}}> Orphanage:</div>
+         <div style={{fontSize:"1rem"}}> {organization}</div></div>
         <div style={{fontSize:"1rem",textAlign:"left"}}>
-         Address: {address}
+        <div> Address:
+        </div>       <div style={{fontSize:"0.8rem"}}> {address}</div>
+        </div>
+        <div style={{fontSize:"1rem",textAlign:"left"}}>
+         City: {city}
+        </div>
+        <div style={{fontSize:"1rem",textAlign:"left"}}>
+         State: {state}
         </div>
        
       </Card.Body>
